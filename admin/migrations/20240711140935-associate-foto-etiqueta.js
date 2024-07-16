46884666ba46c911ec8131f23564e7ae6cad6c17
Jorge Arboleda <jarboleda@justcapital.media>
2024-07-16 07:24:26 -0500

    taller2p2

---
+++ admin/migrations/20240711140935-associate-foto-etiqueta.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addConstraint('fotoetiquetas', {
+      fields: ['foto_id'],
+      name: 'foto_id_fk',
+      type: 'foreign key',
+      references: {
+      table: 'fotos',
+      field: 'id'
+      },
+      onDelete: 'cascade',
+      onUpdate: 'set null'
+      });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.addConstraint('fotoetiquetas', {
+      fields: ['etiqueta_id'],
+      name: 'etiqueta_id_fk',
+      type: 'foreign key',
+      references: {
+      table: 'etiquetas',
+      field: 'id'
+      },
+      onDelete: 'cascade',
+      onUpdate: 'set null'
+      });
+  }
+};
